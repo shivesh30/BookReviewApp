d817876b007bece00a02c4d26f8e9a4c665f1fdc
shivesh30 <shiveshkumar30@gmail.com>
2022-04-27 18:44:07 +0530

    book listing page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,30 @@
 class BooksController < ApplicationController
 	def index
+		@books = Book.all.order("created_at DESC")
 	end
+
+	def new
+		@book = Book.new
+	end
+
+	def Create
+		@book =book.new(book_params)
+
+		if @book.save
+			redirect_to_root_path
+		else render 'new'
+		end
+	end
+
+	private
+
+		def book_params
+			params.require(:book).permit(:title,:description,:author)
+	end
+
+
+
+
+
+
 end
